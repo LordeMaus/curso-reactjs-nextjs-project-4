2fc7fba6bc02ae75ea034c11bdeda5a024ab248d
lordemaus <mauricioalvesnutri@gmail.com>
2021-08-03 21:46:26 -0300

    update

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './templates/App';
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyles } from './styles/global-styles';
+import Home from './templates/Home';
+import { theme } from './styles/theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Home />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,14 @@
+import { Heading } from '../../components/Heading';
+import * as Styled from './styles.js';
+
+function Home() {
+  return (
+    <div className="App">
+      <Styled.Wrapper>
+        <Heading>Um texto qualquer</Heading>
+      </Styled.Wrapper>
+    </div>
+  );
+}
+
+export default Home;
