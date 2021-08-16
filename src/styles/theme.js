42f699cda408e6190e0b28a67e54aa3eb35f7cf2
lordemaus <mauricioalvesnutri@gmail.com>
2021-08-16 16:35:44 -0300

    ajustes finais

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -11,7 +11,7 @@ export const theme = {
       secondary: "'Montserrat', sans-serif",
     },
     size: {
-      xsmall: '.8rem',
+      xsmall: '8rem',
       small: '1.6rem',
       medium: '2.4rem',
       large: '3.2rem',
@@ -20,6 +20,9 @@ export const theme = {
       huge: '5.6rem',
       xhuge: '6.4rem',
     },
+  },
+  media: {
+    lteMedium: '(max-width: 768px)',
   },
   spacing: {
     xsmall: '8rem',
@@ -30,8 +33,5 @@ export const theme = {
     xxlarge: '4.8rem',
     huge: '5.6rem',
     xhuge: '6.4rem',
-  },
-  media: {
-    lteMedium: '(max-width: 768px)',
   },
 };
